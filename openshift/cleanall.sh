c4e746220a8b6ccced27122377314cab596658bd
saga93 <carlosgsagala@gmail.com>
2019-05-29 18:37:00 -0300

    fixes

---
+++ openshift/cleanall.sh
@@ -0,0 +1,4 @@
+
+oc delete project spring-dev
+oc delete project spring-prod
+oc delete project jenkins
--- openshift/start.sh
+++ openshift/start.sh
@@ -2,7 +2,8 @@ oc login -u admin -p redhat
 oc new-project spring-dev
 oc new-project spring-prod
 oc new-project jenkins
-
+oc tag openshift/jenkins:2 jenkins/jenkins:2
+oc new-app jenkins-ephemeral -n jenkins
 while : ; do
   echo "cerificando si Jenkins esta listo..."
   AVAILABLE_REPLICAS=$(oc get dc jenkins -n jenkins -o=jsonpath='{.status.availableReplicas}')
@@ -14,10 +15,8 @@ while : ; do
   sleep 10
 done
 
-oc create -f template.yml -n jenkins
-oc new-app jenkins-persistent -n jenkins
+oc create -f ./pipeline/pipeline.yml -n jenkins
+
 
 oc adm policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n spring-dev
 oc adm policy add-role-to-user edit system:serviceaccount:jenkins:jenkins -n spring-prod
-
-oc new-app pipeline-template -p APP_NAME=spring -p GIT_REPO=https://github.com/carlossagala/opendevsolutions-workshop-pipelines-.git -p GIT_BRANCH=master
